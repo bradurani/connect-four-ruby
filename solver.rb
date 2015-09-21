c2a4ace65db3ca5ff28fd05a7e191684c6530c12
Brad Urani <bradurani@gmail.com>
2015-09-21 05:41:18 +0000

    Create level scorer

--- solver.rb
+++ solver.rb
@@ -1,19 +1,74 @@
 require 'hamster'
+require 'matrix'
+require 'pry'
 
 board = <<-BOARD
-          OOOOOOO
-          OOOOOOO
-          OOOOOOO
-          OOOOOOO
-          OOOOOOO
-          OOOOOOO
+          ROOOOOO
+          BBBOORO
+          RROBOOO
+          BOOBOOO
+          OOOBOOO
+          OOOBOOO
         BOARD
 
-def level_array(level_string)
-  level_string.split(/\n/).map { |row| row.strip.scan(/./) }
+def board_matrix(level_string)
+  Matrix.rows(level_string.split(/\n/).map { |row| row.strip.scan(/./) })
 end
 
-level = level_array(board)
+def board_list(vectors)
+  Hamster::Vector.new(vectors.map do |row|
+    Hamster::Vector.new(row)
+  end)
+end
 
+def boards(matrix)
+  {
+    horizontal: board_list(matrix.row_vectors),
+    vertical: board_list(matrix.column_vectors) 
+  }
+end
 
-puts 
+def boards_score(boards)
+  horizontal = board_score(boards[:horizontal])
+  if(horizontal != 0)
+    horizontal
+  else
+    board_score(boards[:vertical])
+  end
+end
+
+def board_score(board)
+  board.reduce(0) do |score, row|
+    if(score != 0)
+      score
+    else
+      row = row_score(row)
+      if (row >= 4)
+        1
+      elsif (row <= -4)
+        -1
+      else
+        0
+      end
+    end
+  end
+end
+
+def row_score(row)
+  row.reduce(0) do |score, position|
+    if(score == 4 || score == -4)
+      score
+    elsif(position == 'R' && score >= 0)
+      score + 1
+    elsif(position == 'B' && score <= 0)
+      score - 1
+    else
+      0
+    end
+  end
+end
+
+board = boards(board_matrix(board))
+score = boards_score(board)
+
+puts score
