6066f8ae312161cee72f7071353cb949b3821e1c
Brad Urani <bradurani@gmail.com>
2015-09-22 10:43:52 +0000

    Start flattening algo

--- solver.rb
+++ solver.rb
@@ -1,74 +1,104 @@
 require 'hamster'
 require 'matrix'
+require 'colorize'
 require 'pry'
 
-board = <<-BOARD
-          ROOOOOO
-          BBBOORO
-          RROBOOO
-          BOOBOOO
-          OOOBOOO
-          OOOBOOO
-        BOARD
+starting_board = <<-BOARD
+                  OOOOOOO
+                  OOOOOOO
+                  RYRYOOO
+                  YYYROOO
+                  RRRROOO
+                  YYRRROO
+                BOARD
+
+def flatten_tree_lazy(board_stream, piece)
+  next_piece = piece == 'Y' ? 'R' : 'Y'
+  next_tier = board_stream.flat_map { |b| possible_next_moves(b, piece)}
+  board_stream.concat(Hamster::Stream.new { flatten_tree_lazy(next_tier, next_piece)})
+end
+
+def possible_next_moves(board, piece)
+  (0...board.first.length).map do |n|
+    add_piece_to_column(board, n, piece)
+  end.compact
+end
+
+def add_piece_to_column(board, n, piece)
+  top_index = horizontal_highest_piece(board, n)
+  return nil if top_index == 0
+  board.set(top_index - 1, board[top_index - 1].set(n, piece))
+end
+
+def horizontal_highest_piece(board, n)
+  board.map { |row| row[n] }.index { |char| char != 'O' } || board.length
+end
+
+#### Detect Winner ####
+
+def board_score(board)
+  horizontal_winner(board) || vertical_winner(board)
+end
+
+def horizontal_winner(board)
+  board.reduce(nil) do |winner, row| 
+    winner || vector_winner(row)
+  end
+end
+
+def vertical_winner(board)
+  (0...board.first.length).reduce(nil) do |winner, n| 
+    winner || vector_winner(board.map { |row| row[n] })
+  end
+end
+
+def vector_winner(row)
+  winning_chunk = row.chunk(&:itself)
+                     .detect do |tuple| 
+                        tuple[0] != 'O' && tuple[1].length >= 4
+                      end
+  winning_chunk ? winning_chunk[0] : nil
+end
+
+
+
+#### SETUP ####
 
 def board_matrix(level_string)
   Matrix.rows(level_string.split(/\n/).map { |row| row.strip.scan(/./) })
 end
 
-def board_list(vectors)
-  Hamster::Vector.new(vectors.map do |row|
+def board_list(matrix)
+  Hamster::Vector.new(matrix.row_vectors.map do |row|
     Hamster::Vector.new(row)
   end)
 end
 
-def boards(matrix)
-  {
-    horizontal: board_list(matrix.row_vectors),
-    vertical: board_list(matrix.column_vectors) 
-  }
-end
+#### PRETTY PRINT ####
 
-def boards_score(boards)
-  horizontal = board_score(boards[:horizontal])
-  if(horizontal != 0)
-    horizontal
-  else
-    board_score(boards[:vertical])
+def pp(board)
+  board.reduce('') do |acc, row|
+    acc + row.map { |piece| color(piece) }.join + "\n"
   end
 end
 
-def board_score(board)
-  board.reduce(0) do |score, row|
-    if(score != 0)
-      score
-    else
-      row = row_score(row)
-      if (row >= 4)
-        1
-      elsif (row <= -4)
-        -1
-      else
-        0
-      end
-    end
+def color(piece)
+  case piece
+  when 'O'; "●".white
+  when 'R'; '●'.red
+  when 'Y'; '●'.yellow
+  else raise "Unknown char: #{piece}"
   end
 end
 
-def row_score(row)
-  row.reduce(0) do |score, position|
-    if(score == 4 || score == -4)
-      score
-    elsif(position == 'R' && score >= 0)
-      score + 1
-    elsif(position == 'B' && score <= 0)
-      score - 1
-    else
-      0
-    end
+def pp_list(board_list)
+  board_list.reduce('') do |acc, board|
+    acc + pp(board) + "\n"
   end
 end
 
-board = boards(board_matrix(board))
-score = boards_score(board)
+board = board_list(board_matrix(starting_board))
+tree = flatten_tree_lazy(Hamster.seq { [board] })
 
-puts score
+puts tree
+#puts pp_list(possible)
